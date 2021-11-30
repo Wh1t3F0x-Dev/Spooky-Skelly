db893d1582c6eef9cd0566c7a3f6dbe6843349e3
jfmartin1410 <jfmartin1410@ieszaidinvergeles.org>
2021-11-30 15:01:05 -0500

    Version alpha 0.4 de Spooky-Skelly, añadido LittleBrother, tools.py reorganizado correctamente

--- core/menutxt.py
+++ core/menutxt.py
@@ -6,55 +6,55 @@ class menuTxt:
     MENUS GENERALES
     '''
 
-    msgIndex=colors.red + '''
-(+) Tools Menus:''' + colors.lightorange + '''
+    msgIndex=colors.title + '''
+(+) Tools Menus:''' + colors.subtitle + '''
     [1] Simple Information Gathering
     [2] Analysis
     [3] Password & Dicctionaries
-    [4] Social Enginering ''' + colors.red + '''
-(+) Other Options:''' + colors.lightorange + '''
+    [4] Social Enginering ''' + colors.title + '''
+(+) Other Options:''' + colors.subtitle + '''
     [99] Exit the tool
     '''
 
-    msgInfo=colors.red + '''
-(+)InfoGather Tools:''' + colors.lightorange + '''
+    msgInfo=colors.title + '''
+(+)InfoGather Tools:''' + colors.subtitle + '''
     [1] Ping
     [2] Whois
     [3] Tracceroute
     [4] Nslookup
-    [5] Ping + Whois + Tracceroute + Nslookup ''' + colors.red + '''
-(+) Other Options:''' + colors.lightorange + '''
+    [5] Ping + Whois + Tracceroute + Nslookup ''' + colors.title + '''
+(+) Other Options:''' + colors.subtitle + '''
     [6] Delete All Logs
     [99] Go back to Main Menu
     '''
 
-    msgDicc=colors.red + '''
-(+) Dictionaries Tools:''' + colors.lightorange + '''
-    [1] CuPP''' + colors.red + '''
-(+) Password Attack Tools:''' + colors.lightorange + '''
-    [2] Crunch''' + colors.red + '''
-(+) Other Options:''' + colors.lightorange + '''
+    msgDicc=colors.title + '''
+(+) Dictionaries Tools:''' + colors.subtitle + '''
+    [1] CuPP''' + colors.title + '''
+(+) Password Attack Tools:''' + colors.subtitle + '''
+    [2] Crunch''' + colors.title + '''
+(+) Other Options:''' + colors.subtitle + '''
     [3] Delete All Logs
     [99] Go back to Main Menu
     '''
 
-    msgAnalysis=colors.red + '''
-(+) Analysis Tools:''' + colors.lightorange + '''
+    msgAnalysis=colors.title + '''
+(+) Analysis Tools:''' + colors.subtitle + '''
     [1] Nmap
-    [2] Netdiscover''' + colors.red + '''
-(+) Other Options:''' + colors.lightorange + '''
+    [2] Netdiscover''' + colors.title + '''
+(+) Other Options:''' + colors.subtitle + '''
     [3] Delete All Logs
     [99] Go back to Main Menu
     '''
 
-    msgSocial=colors.red + '''
-(+) SocialEnginering Tools: ''' + colors.lightorange + '''
+    msgSocial=colors.title + '''
+(+) SocialEnginering Tools: ''' + colors.subtitle + '''
     [1]
-    [2] ''' + colors.red + '''
-(+) OSINT Tools: ''' + colors.lightorange + '''    
+    [2] ''' + colors.title + '''
+(+) OSINT Tools: ''' + colors.subtitle + '''    
     [3] DelvedLeak 
-    [4] ''' + colors.red + '''
-(+) Other Options:''' + colors.lightorange + '''
+    [4] LittleBrother''' + colors.title + '''
+(+) Other Options:''' + colors.subtitle + '''
     [5] Delete All Logs
     [99] Go back to Main Menu
     '''
@@ -63,12 +63,12 @@ class menuTxt:
     MENUS CONCRETOS
     '''
 
-    msgNmap=colors.red + '''
-(+) Nmap Options:''' + colors.lightorange + '''
+    msgNmap=colors.title + '''
+(+) Nmap Options:''' + colors.subtitle + '''
     [1] Simple Scan
     [2] Port Scan
-    [3] Operatin System Detection ''' + colors.red + '''     
-(+) Other Options:''' + colors.lightorange + '''
+    [3] Operatin System Detection ''' + colors.title + '''     
+(+) Other Options:''' + colors.subtitle + '''
     [99] Go back to Info Gathering Menu
     '''
 
